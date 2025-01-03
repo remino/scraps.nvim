1fd2318f94f1ec2efb42b8e5dba61b9b3e7eb7a0
Rémino Rem <29999+remino@users.noreply.github.com>
2025-01-03 09:03:03 +0900

    Refactor plugin to allow no opts in Lazy

--- lua/scraps/init.lua
+++ lua/scraps/init.lua
@@ -5,7 +5,17 @@ local action_state = require("telescope.actions.state")
 
 vim.g.scraps_dir = vim.fn.expand((os.getenv("XDG_DATA_HOME") or "~/.local/share") .. "/scraps")
 
+local function require_telescope()
+	local status, _ = pcall(require, "telescope")
+	if not status then
+		vim.notify("Telescope is required for Scraps", vim.log.levels.ERROR, { title = "Scraps Error" })
+		return
+	end
+end
+
 local function browse(dir)
+	require_telescope()
+
 	local directory = dir or vim.fn.expand("%:p:h")
 
 	require("telescope.builtin").find_files({
@@ -24,35 +34,34 @@ local function browse(dir)
 	})
 end
 
-function M.setup(opts)
-	opts = opts or {}
-	vim.g.scraps_dir = opts.scraps_dir or vim.g.scraps_dir
-
-	local status, _ = pcall(require, "telescope")
-	if not status then
-		vim.notify("Telescope is required for Scraps", vim.log.levels.ERROR, { title = "Scraps Error" })
+vim.api.nvim_create_user_command("Scraps", function(options)
+	if options.args == "" then
+		browse(vim.g.scraps_dir or vim.fn.expand("%:p:h"))
 		return
 	end
 
-	vim.api.nvim_create_user_command("Scraps", function(options)
-		browse(options.args or vim.g.scraps_dir or vim.fn.expand("%:p:h"))
-	end, {
-		desc = "Browse scraps directory",
-		nargs = "?",
-		complete = "dir",
-	})
+	browse(options.args)
+end, {
+	desc = "Browse scraps directory",
+	nargs = "?",
+	complete = "dir",
+})
 
-	vim.api.nvim_create_user_command("ScrapsConfigDir", function()
-		browse(vim.fn.expand((os.getenv("XDG_CONFIG_HOME") or "~/.config") .. "/nvim"))
-	end, {
-		desc = "Browse the Neovim configuration directory",
-	})
+vim.api.nvim_create_user_command("ScrapsConfigDir", function()
+	browse(vim.fn.expand((os.getenv("XDG_CONFIG_HOME") or "~/.config") .. "/nvim"))
+end, {
+	desc = "Browse the Neovim configuration directory",
+})
 
-	vim.api.nvim_create_user_command("ScrapsCurrentDir", function()
-		browse(vim.fn.expand("%:p:h"))
-	end, {
-		desc = "Browse the current directory",
-	})
+vim.api.nvim_create_user_command("ScrapsCurrentDir", function()
+	browse(vim.fn.expand("%:p:h"))
+end, {
+	desc = "Browse the current directory",
+})
+
+function M.setup(opts)
+	opts = opts or {}
+	vim.g.scraps_dir = opts.scraps_dir or vim.g.scraps_dir
 end
 
 return M
